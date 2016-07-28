603107ddc6b4e5c85a6cc9d8b9b7555a4b4fa32a
Aleksey Razorvin <arazor72@gmail.com>
2016-07-28 23:51:29 +0200

    validate model and CRUD card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,18 @@
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, presence: true
+  
+  validate :original_translated_text_are_different
+  def original_translated_text_are_different
+    if self.original_text == self.translated_text
+      errors.add(:original_text, 'должен отличаться')
+      errors.add(:translated_text, 'должен отличаться')
+    end
+  end
+
+  before_save :set_date_review
+
+  def set_date_review
+    self.review_date = Time.now.to_date + 3
+  end 
+
 end
