6f5a9683341b7ee55f39feb0d77dac2710869329
Aleksey Razorvin <arazor72@gmail.com>
2016-07-30 23:53:16 +0200

    very DRY seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# second source of words
-# doc = Nokogiri::HTML(open("http://en365.ru/top1000.htm"))
-
 require 'open-uri'
 doc = Nokogiri::HTML(open("http://w2mem.com/words/en/1/"))
+parsing_page_forms = doc.css('.row')
 
-# tried in rails consol
-# original_text = doc.css('input.input-sm')[4].attributes["value"].text
-# translated_text = doc.css('input.input-sm')[5].attributes["value"].text
-
-parsing_page_forms = doc.css('input.input-sm')
-
-original_text = ''; translated_text = ''
-
-parsing_page_forms.each_with_index do |word, index|
-  if (index % 2) == 0
-    original_text = word.attributes["value"].text
-  else
-    translated_text = word.attributes["value"].text
-    Card.create(original_text: original_text, translated_text: translated_text)
-    original_text = ''; translated_text = ''
+parsing_page_forms.drop(3).each do |pp|
+  if !pp.css('input.input-sm')[0].nil?
+    Card.create(original_text: pp.css('input.input-sm')[0].attributes["value"].text, 
+                translated_text: pp.css('input.input-sm')[1].attributes["value"].text)
   end
 end
 
